1a6144c9c50530b03046913e4c8d150f14a74f63
igaaoo <igorneves0404@gmail.com>
2025-01-26 14:49:12 -0300

    Feature | Integrate Swagger for API documentation

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,29 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiParam, ApiQuery, ApiBody, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
   @Post('login')
+  @ApiBody(
+    {
+      schema: {
+        type: 'object',
+        properties: {
+          email: {
+            type: 'string',
+            example: 'igor@gmail.com',
+          },
+          password: {
+            type: 'string',
+            example: '123456',
+          },
+        }
+      }
+    }
+  )
   async login(@Body() body: { email: string; password: string; }) {
     const user = await this.authService.validateUser(body.email, body.password);
     return this.authService.login(user);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = new Logger('Bootstrap');
   app.useLogger(logger);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste Nextek')
+    .setDescription('Documentação para a API de tarefas')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(4000);
   logger.log(`Application running on: http://localhost:4000`);
